a9d42231e3f2fa9ef4a127dca798a87d1aa098b9
Chukwudi Derek Uche <uchederek@yahoo.com>
2017-02-15 12:11:56 -0500

    Added node types and add node

--- Perceptron.py
+++ Perceptron.py
@@ -1,3 +1,37 @@
 class Perceptron(object):
+    def __init__(self, previous_layer=[]):
+        self.previous_layer = previous_layer
+        self.next_layer = next_layer
+
+        for n in self.previous_layer:
+            n.next_layer.append(self)
+
+        self.value = None
+
+    def move_forward(self):
+        raise NotImplemented
+
+
+
+class Input(Perceptron):
     def __init__(self):
-        pass #LOL for now
+        Perceptron.__init__(self)
+
+    def move_forward(self):
+        if value is not None:
+            self.value = value
+
+class Add(Perceptron):
+    def __init__(self, x, y):
+        Node.__init__(self, [x, y])
+
+    def move_forward(self):
+
+def forward(output, sorted_nodes):
+    for node in sorted_nodes:
+        node.move_forward()
+
+    return output.value()
+
+def topological_sort(node_dict):
+    
